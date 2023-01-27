721c203c493507cb324474e77be4915ff432278e
Anna Saratovskaia <annja@list.ru>
2023-01-27 09:41:15 -0800

    change ui, link game logic to DOM

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let btnRock = document.querySelector('.rock');
+let btnPaper = document.querySelector('.paper');
+let btnScissors = document.querySelector('.scissors');
+
+
+
 function getComputerChoice() {
     let random = Math.trunc(Math.random() * 10);
     let computerChoice;
@@ -19,6 +25,38 @@ function getPlayerCoice() {
 
 }
 
+btnPaper.addEventListener('click', function() {
+    let computerSelection = getComputerChoice();
+    if (computerSelection === 'paper') {
+        alert(`Draw! You both chosen ${computerSelection}.`)
+    } else if (computerSelection === 'rock') {
+        alert(`You win! Paper beats rock. `)
+    } else if(computerSelection === 'scissors') {
+        alert (`You lose! Scissors beat paper`)
+    }
+})
+
+btnRock.addEventListener('click', function() {
+    let computerSelection = getComputerChoice();
+    if (computerSelection === 'rock') {
+        alert(`Draw! You both chosen rock.`)
+    } else if (computerSelection === 'paper') {
+        alert(`You lose! Paper beats rock`)
+    } else if (computerSelection === 'scissors') {
+        alert (`You win! Rock beats scissors.`)
+    }
+})
+
+btnScissors.addEventListener('click', function () {
+    let computerSelection = getComputerChoice();
+    if (computerSelection === 'scissors') {
+        alert(`Draw! You both chosen ${computerSelection}.`)
+    } else if (computerSelection === 'rock') {
+        alert (`You lose! Rock beats scissors`)
+    }  else if (computerSelection === 'paper') {
+        alert (`You win! Scissors beat paper.`)
+    } 
+})
 
 function playRound() {
     let computerSelection = getComputerChoice();
@@ -44,4 +82,3 @@ let playerSelection = getPlayerCoice();
     }
 }
 
-playRound()
