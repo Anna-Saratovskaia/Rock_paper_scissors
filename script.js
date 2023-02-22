34b8f541f03c6d4aac477298ae03b9b46b0e88ea
Anna Saratovskaia <annja@list.ru>
2023-02-22 09:52:32 -0800

    Add main game logic

--- script.js
+++ script.js
@@ -2,68 +2,70 @@ let btnRock = document.querySelector('.rock');
 let btnPaper = document.querySelector('.paper');
 let btnScissors = document.querySelector('.scissors');
 let result = document.querySelector('.result h2');
-let btnPlay = document.querySelector('.play');
-let currentRound = document.querySelector('.current-round');
-let computerScore = document.querySelector('.computer-score');
-let playerScore = document.querySelector('.player-score')
+let btnPlay = document.querySelector('.play-btn');
+let currentRoundEl = document.querySelector('h3 p');
+let computerScoreEl = document.querySelector('.computer-score');
+let playerScoreEl = document.querySelector('.player-score');
+let btnsEl = document.querySelectorAll('.btns');
 
 
-function play () {
-    
-}
+let playing = true;
+let computerScore = 0;
+let playerScore = 0;
+let currentRound = 0;
+
+
+btnsEl.forEach(btn => {
+    btn.addEventListener('click', function(){
+        let computerChoice = getComputerChoice()
+        console.log(computerChoice)
+        let playerChoice = btn.dataset.playerChoice;
+        currentRound++;
+        currentRoundEl.textContent = `${currentRound.toString()}`
+
+        if(computerChoice === playerChoice){
+            result.textContent = `The result of this round is: draw. You both threw ${computerChoice}`
+        }  else if (computerChoice === 'rock' && playerChoice === 'paper') {
+            result.textContent = `The result of this round is: You win! Paper beats rock.`; 
+            playerScore++
+            playerScoreEl.textContent = `${playerScore.toString()}`
+        } else if(computerChoice === 'scissors' && playerChoice === 'paper') {
+            result.textContent = `The result of this round is: You lose! Scissors beat paper`;
+            computerScore++;
+            computerScoreEl.textContent = `${computerScore.toString()}`
+        } else if (computerChoice === 'rock' && playerChoice === 'scissors') {
+            result.textContent = `The result of this round is: You lose! Rock beats scissors`;
+            computerScore++;
+            computerScoreEl.textContent = `${computerScore.toString()}`
+        }else if(computerChoice === 'paper' && playerChoice === 'scissors') {
+            result.textContent = `The resilt of this round is: You win! Scissors beats paper`;
+            playerScore++
+            playerScoreEl.textContent = `${playerScore.toString()}`
+        } else if (computerChoice === 'scissors' && playerChoice === 'rock') {
+            result.textContent = 'The result of this round is: You win! Rock beats scissors';
+            playerScore++
+            playerScoreEl.textContent = `${playerScore.toString()}`
+        } else {
+            result.textContent = 'The result of this round is: you lose! Paper beats rock';
+            computerScore++;
+            computerScoreEl.textContent = `${computerScore.toString()}`
+        }
+    }) 
+
+})
+
+
 function getComputerChoice() {
-    let random = Math.trunc(Math.random() * 10);
+    let random = Math.ceil(Math.random() * 10);
     let computerChoice;
     if(random <= 3) {
-        computerChoice = 'rock'
-    } else if (random > 3 && random <= 6) {
-        computerChoice = 'paper'
+        computerChoice = 'rock';
+    } else if(random > 3 && random <= 7) {
+        computerChoice = 'paper';
     } else {
-        computerChoice = "scissors"
+        computerChoice = 'scissors'
     }
-
-    return computerChoice
+    return computerChoice;
 }
-
-function getPlayerCoice() {
-
-   let playerChoice = prompt('What is your choice?')
-   return playerChoice.toLowerCase()
-
-}
-
-btnPaper.addEventListener('click', function() {
-    let computerSelection = getComputerChoice();
-    if (computerSelection === 'paper') {
-        result.textContent = `The result of this round is: Draw! You both chosen ${computerSelection}.`
-    } else if (computerSelection === 'rock') {
-        result.textContent = `The result of this round is: You win! Paper beats rock.`
-    } else if(computerSelection === 'scissors') {
-        result.textContent = `The result of this round is: You lose! Scissors beat paper`
-    }
-})
-
-btnRock.addEventListener('click', function() {
-    let computerSelection = getComputerChoice();
-    if (computerSelection === 'rock') {
-        result.textContent = `The result of this round is: Draw! You both chosen rock.`
-    } else if (computerSelection === 'paper') {
-        result.textContent = `The result of this round is: You lose! Paper beats rock`
-    } else if (computerSelection === 'scissors') {
-        result.textContent = `The result of this round is: You win! Rock beats scissors.`
-    }
-})
-
-btnScissors.addEventListener('click', function () {
-    let computerSelection = getComputerChoice();
-    if (computerSelection === 'scissors') {
-        result.textContent = `The result of this round is: Draw! You both chosen ${computerSelection}.`
-    } else if (computerSelection === 'rock') {
-        result.textContent = `The result of this round is: You lose! Rock beats scissors`
-    }  else if (computerSelection === 'paper') {
-        result.textContent = `The result of this round is: You win! Scissors beat paper.`
-    } 
-})
-
 
 
