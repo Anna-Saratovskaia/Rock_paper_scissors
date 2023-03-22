dd88723678ed2e9de6860d921161dcdc79f218dd
Anna Saratovskaia <annja@list.ru>
2023-03-22 16:07:26 -0700

    Present for Roman

--- script.js
+++ script.js
@@ -1,57 +1,87 @@
-let btnRock = document.querySelector('.rock');
-let btnPaper = document.querySelector('.paper');
-let btnScissors = document.querySelector('.scissors');
-let result = document.querySelector('.result h2');
+let resultParent = document.querySelector('.result');
 let btnPlay = document.querySelector('.play-btn');
 let currentRoundEl = document.querySelector('h3 p');
 let computerScoreEl = document.querySelector('.computer-score');
 let playerScoreEl = document.querySelector('.player-score');
 let btnsEl = document.querySelectorAll('.btns');
+let computerChoiceEl = document.querySelector('.computer-choice');
+let winEl = document.querySelector('winner');
 
 
-let playing = true;
-let computerScore = 0;
-let playerScore = 0;
-let currentRound = 0;
+let playing, computerScore, playerScore, currentRound;
 
+let result = document.createElement('p');
+
+function  init () {
+    playing = true;
+    computerScore = 0;
+    playerScore = 0;
+    currentRound = 0;
+
+    result.textContent = '';
+    currentRoundEl.textContent = `0`;
+    playerScoreEl.textContent = `0`;
+    computerScoreEl.textContent = `${computerScore.toString()}`
+}
+
+init()
+
+let play = function(event){
+    let btn = event.target;
+    if (!playing) return;
+    let computerChoice = getComputerChoice()
+    let playerChoice = btn.dataset.playerChoice;
+    currentRound++;
+    if(currentRound >= 5) playing = false;
+    
+    currentRoundEl.textContent = `${currentRound.toString()}`
+    computerChoiceEl.textContent = `${computerChoice}`
+
+    // if(currentRound >= 5) {
+    //     btnsEl.forEach(btn => btn.removeEventListener('click',  play))
+    // }
+           
+    if(computerChoice === playerChoice){
+        result.textContent = ` draw. You both threw ${computerChoice}`
+        
+    }  else if (computerChoice === 'rock' && playerChoice === 'paper') {
+        result.textContent = ` You win! Paper beats rock.`; 
+        
+        playerScore++
+        playerScoreEl.textContent = `${playerScore.toString()}`
+    } else if(computerChoice === 'scissors' && playerChoice === 'paper') {
+        result.textContent = ` You lose! Scissors beat paper`;
+        
+        computerScore++;
+        computerScoreEl.textContent = `${computerScore.toString()}`
+    } else if (computerChoice === 'rock' && playerChoice === 'scissors') {
+        result.textContent = ` You lose! Rock beats scissors`;
+        
+        computerScore++;
+        computerScoreEl.textContent = `${computerScore.toString()}`
+    }else if(computerChoice === 'paper' && playerChoice === 'scissors') {
+        result.textContent = ` You win! Scissors beats paper`;
+        resultParent.append(result)
+        
+        playerScore++
+        playerScoreEl.textContent = `${playerScore.toString()}`
+    } else if (computerChoice === 'scissors' && playerChoice === 'rock') {
+        result.textContent = ' You win! Rock beats scissors';
+        
+        playerScore++
+        playerScoreEl.textContent = `${playerScore.toString()}`
+    } else {
+        result.textContent = ' You lose! Paper beats rock';
+        
+        computerScore++;
+        computerScoreEl.textContent = `${computerScore.toString()}`
+    } 
+    
+}
 
 btnsEl.forEach(btn => {
-    btn.addEventListener('click', function(){
-        let computerChoice = getComputerChoice()
-        console.log(computerChoice)
-        let playerChoice = btn.dataset.playerChoice;
-        currentRound++;
-        currentRoundEl.textContent = `${currentRound.toString()}`
-
-        if(computerChoice === playerChoice){
-            result.textContent = `The result of this round is: draw. You both threw ${computerChoice}`
-        }  else if (computerChoice === 'rock' && playerChoice === 'paper') {
-            result.textContent = `The result of this round is: You win! Paper beats rock.`; 
-            playerScore++
-            playerScoreEl.textContent = `${playerScore.toString()}`
-        } else if(computerChoice === 'scissors' && playerChoice === 'paper') {
-            result.textContent = `The result of this round is: You lose! Scissors beat paper`;
-            computerScore++;
-            computerScoreEl.textContent = `${computerScore.toString()}`
-        } else if (computerChoice === 'rock' && playerChoice === 'scissors') {
-            result.textContent = `The result of this round is: You lose! Rock beats scissors`;
-            computerScore++;
-            computerScoreEl.textContent = `${computerScore.toString()}`
-        }else if(computerChoice === 'paper' && playerChoice === 'scissors') {
-            result.textContent = `The resilt of this round is: You win! Scissors beats paper`;
-            playerScore++
-            playerScoreEl.textContent = `${playerScore.toString()}`
-        } else if (computerChoice === 'scissors' && playerChoice === 'rock') {
-            result.textContent = 'The result of this round is: You win! Rock beats scissors';
-            playerScore++
-            playerScoreEl.textContent = `${playerScore.toString()}`
-        } else {
-            result.textContent = 'The result of this round is: you lose! Paper beats rock';
-            computerScore++;
-            computerScoreEl.textContent = `${computerScore.toString()}`
-        }
-    }) 
-
+    btn.addEventListener('click', play);
+    resultParent.append(result);
 })
 
 
@@ -69,3 +99,5 @@ function getComputerChoice() {
 }
 
 
+
+btnPlay.addEventListener('click', init);
